808c7726947e2e95eebb42df7321f44e3f62fca5
yuji0208 <yuji2080jtcxs@icloud.com>
2020-05-05 00:18:02 +0900

    2章

---
+++ config/routes.rb
@@ -0,0 +1,5 @@
+# アプリケーションのルーティングを定義
+Rails.application.routes.draw do
+  resources :users
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
